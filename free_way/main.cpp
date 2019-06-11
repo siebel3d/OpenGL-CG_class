670394be106654921ed0be2331d557b1c1f8d0c5
siebel3d <siebel3d@outlook.com>
2019-06-11 09:23:04 -0300

    new file:   free_way/main 	modified:   free_way/main.cpp 	new file:   texture_render/Makefile 	new file:   texture_render/SOIL.h 	new file:   texture_render/main 	new file:   texture_render/main.cpp 	new file:   texture_render/main.cpp.save 	new file:   texture_render/src/jequiti.jpg 	new file:   texture_render/src/jequiti.png

---
+++ free_way/main.cpp
@@ -0,0 +1,191 @@
+#include<GL/glew.h>
+#include<GL/glut.h>
+#include<GLFW/glfw3.h>
+#include<GL/freeglut.h>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sstream>
+#include<unistd.h>
+
+//Window size
+#define windowH 900
+#define windowW 1600
+
+float PI = atanf(1.0f)*4.0f;
+
+struct Car{
+	int posX;
+	int posY;
+	int speed;
+	int sizeX;
+	int sizeY;
+};
+
+struct Player{
+	int score;
+	int playerPosX;
+	int playerPosY;
+	int sizeX;
+	int sizeY;
+	int laneFlag;
+};
+
+struct Player p1;
+struct Player p2;
+struct Car c1;
+struct Car c2;
+struct Car c3;
+struct Car c4;
+struct Car c5;
+struct Car c6;
+struct Car c7;
+struct Car c8;
+struct Car c9;
+struct Car c10;
+
+//Variables for circle drawing
+GLfloat circ_pnt = 10;
+GLfloat ang, ballAxis=10;
+
+//Colors
+float black = 0.0f;
+float white = 1.0f;
+
+//Variables for game scale
+float scaleTV = 1;
+int intervalT= 1000/500;
+
+void display(void);
+void screen(GLsizei w, GLsizei h);
+void keyPressed(unsigned char key, int x, int y);
+void keyUp(unsigned char key, int x, int y);
+void keyOperations(void);
+void mouseMovement(int x, int y);
+void update(int value);
+void updateBall();
+bool* keyStates = new bool[256];
+
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(windowW, windowH);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("OpenGL Free Way");
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glutFullScreen();
+	glutReshapeFunc(screen);
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyPressed);
+	glutKeyboardUpFunc(keyUp);
+	glutTimerFunc(intervalT, update, 0);
+	glutMainLoop();
+	return(0);
+}
+
+void road(){
+
+}
+
+void keyPressed(unsigned char key, int x, int y){
+	if(key == 'w') keyStates[key] = true;
+	if(key == 's') keyStates[key] = true;
+	if(key == 'o') keyStates[key] = true;
+	if(key == 'l') keyStates[key] = true;
+	switch(key){
+		case 27:
+			glutDestroyWindow (0);
+			exit(0);
+			break;
+	}
+}
+
+void keyUp(unsigned char key, int x, int y){
+	if(key == 'w') keyStates[key] = false;
+	if(key == 's') keyStates[key] = false;
+	if(key == 'o') keyStates[key] = false;
+	if(key == 'l') keyStates[key] = false;
+}
+
+void keyOperations(void){
+	/*if((keyStates['w'])&&((p1.paddlePos<=paddleYlimit+paddleSpeed)&&(p1.paddlePos>=-(paddleYlimit+paddleSpeed)))){
+		p1.paddlePos-=paddleSpeed;
+	}
+	if((keyStates['s'])&&((p1.paddlePos<=paddleYlimit-paddleSpeed)&&(p1.paddlePos<=(paddleYlimit+paddleSpeed)))){
+		p1.paddlePos+=paddleSpeed;
+	}
+	if((keyStates['o'])&&((p2.paddlePos<=paddleYlimit+paddleSpeed)&&(p2.paddlePos>=-(paddleYlimit+paddleSpeed)))){
+		p2.paddlePos-=paddleSpeed;
+	}
+	if((keyStates['l'])&&((p2.paddlePos<=paddleYlimit-paddleSpeed)&&(p2.paddlePos<=(paddleYlimit+paddleSpeed)))){
+		p2.paddlePos+=paddleSpeed;
+	}*/
+}
+
+void drawText(float x, float y, int font, std::string text, int r, int g, int b) {
+    glColor3ub(r,g,b);
+    glRasterPos2f(x, y);
+    if(font==1){
+    	glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, (const unsigned char*)text.c_str());
+    }
+    if(font==2){
+    	glutBitmapString(GLUT_BITMAP_HELVETICA_12, (const unsigned char*)text.c_str());
+    }    
+}
+
+std::string int2str(int x){
+	std::stringstream ss;
+	ss << x;
+	return ss.str( );
+}
+
+void screenText(){
+	drawText(-(windowW/4),-420, 1, int2str(p1.score), 255, 255, 255);
+	drawText(windowW/4,-420, 1, int2str(p2.score), 255, 255, 255);
+	drawText(-(windowW/2)+200, 420, 2, "S3D", 226,114,9);
+}
+
+
+void updateBall(){
+	
+}
+
+void update(int value){
+	updateBall();
+	glutPostRedisplay();
+	glutTimerFunc(intervalT, update, 0);
+}
+
+void draw_elements(){
+	glLoadIdentity();
+	glPushMatrix();
+		glTranslatef((windowW)/2,(windowH)/2,0);
+		glScalef(scaleTV,-(scaleTV),scaleTV);
+		screenText();
+
+		if((p1.score==10)||(p2.score==10)){
+			glutDestroyWindow(0);
+			exit(0);
+		}
+		glutPostRedisplay();
+	glPopMatrix();
+}
+
+void display(){
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	keyOperations();
+	glClearColor(black,black,black,white);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glViewport(0,0,windowW, windowH);
+	draw_elements();
+	glFlush();
+}
+
+void screen(GLsizei w, GLsizei h){
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, windowW, 0, windowH);
+	glMatrixMode(GL_MODELVIEW);
+}
---
+++ texture_render/main.cpp
@@ -0,0 +1,181 @@
+#include<GL/glew.h>
+#include<GL/glut.h>
+#include<GLFW/glfw3.h>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<SOIL/SOIL.h>
+
+#define windowH 900
+#define windowW 1600
+
+float PI = atanf(1.0f)*4.0f;
+
+struct Vertices{
+	int x;
+	int y;
+};
+
+//Variables for circle drawing
+GLfloat circ_pnt = 200;
+GLfloat ang, Xaxis, Yaxis, ballAxis;
+
+//position variables
+struct Vertices v1;
+struct Vertices v2 = {.x=50, .y=150};
+
+//Colors
+float black = 0.0f;
+float white = 1.0f;
+float ballColor = white;
+float grey = 0.5f;
+
+//Variables for ball colision
+float a_value;
+float b_value;
+float c_value;
+bool touch=false;
+
+
+void display(void);
+void screen(GLsizei w, GLsizei h);
+void keyboard(unsigned char key, int x, int y);
+void mouseMovement(int x, int y);
+
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+	//glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(windowW, windowH);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Circle Events");
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glutFullScreen();
+	glutReshapeFunc(screen);
+	glutDisplayFunc(display);
+	glutKeyboardFunc(&keyboard);
+	glutPassiveMotionFunc(mouseMovement);
+	glutMainLoop();
+
+	return(0);
+}
+
+void keyboard(unsigned char key, int x, int y){
+	switch(key){
+		case 'b':
+			ballColor = black;
+			break;
+		case 'w':
+			break;
+		case 's':
+			break;
+		case 'o':
+			break;
+		case 'l':
+			break;
+		case 27:
+			glutDestroyWindow (0);
+			exit(0);
+			break;
+	}
+}
+
+void mouseMovement(int x, int y){
+	v1.x = x-(windowW/2);
+	v1.y = y-(windowH/2);
+
+	float a_value=v1.x-v2.x;
+	float b_value=v1.y-v2.y;
+	float c_value=sqrt((a_value*a_value)+(b_value*b_value));
+
+	if(c_value < (ballAxis*2)){
+		touch=true;
+		glEnable(GL_TEXTURE_2D);
+	}else{
+		touch=false;
+		glDisable(GL_TEXTURE_2D);
+	}
+
+	printf("\nX: %i", v1.x);
+	printf("\nY: %i", v1.y);
+}
+
+void ball(){
+	glColor3f(ballColor,ballColor,ballColor);
+	ballAxis = 30;
+	glBegin(GL_POLYGON);
+		for(int i=0;i<circ_pnt;i++){
+			ang=(2*PI*i)/circ_pnt;
+			glVertex2f(cos(ang)*ballAxis,sin(ang)*ballAxis);
+		}
+	glEnd();
+}
+
+void gameOverScreen(){
+	GLuint tex_2d = SOIL_load_OGL_texture
+	(
+		"src/jequiti.png",
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+	);
+
+	if( 0 == tex_2d ){
+	printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+	}
+
+	glBindTexture(GL_TEXTURE_2D, tex_2d);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f,0.0f);        glVertex2f(800, 450);
+	glTexCoord2f(1.0f,1.0f);        glVertex2f(800, -450);
+	glTexCoord2f(0.0f,1.0f);        glVertex2f(-800, -450);
+	glTexCoord2f(0.0f,0.0f);        glVertex2f(-800, 450);
+	glEnd();
+}
+
+void draw_elements(){
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef((windowW)/2,(windowH)/2,0);
+	glScalef(1,-1,1);
+	glPushMatrix();
+	glTranslatef(v2.x,v2.y,0);
+	ballColor=grey;
+	ball();
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(v1.x,v1.y,0);
+	ballColor=white;
+	ball();
+	glPopMatrix();
+
+	if(touch==true){
+		gameOverScreen();
+	}
+
+	glutPostRedisplay();
+	glPopMatrix();
+}
+
+void display(){
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glClearColor(black,black,black,white);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glViewport(0,0,windowW, windowH);
+	draw_elements();
+
+	glutSwapBuffers();
+}
+
+void screen(GLsizei w, GLsizei h){
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluOrtho2D(0, windowW, 0, windowH);
+	glMatrixMode(GL_MODELVIEW);
+
+}
